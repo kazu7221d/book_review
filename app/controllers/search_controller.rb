e45f5d005867319c6c3c1d990b8d4ecd215d9706
kazu7221d <mkz.dm0@gmail.com>
2018-01-08 22:03:43 +0900

    楽天APIテスト

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
   def search
     if params[:keyword]
+      puts "rakuten start"
       #items = RakutenWebService::Books::Book.search(:keyword => params[:keyword])
       items = RakutenWebService::Ichiba::Item.search(:keyword => params[:keyword])
       p items
+      puts "rakuten end"
       render 'search'
     else
       items = nil
