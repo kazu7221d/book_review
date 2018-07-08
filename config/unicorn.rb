1902b57517254b10131848b41484523f87cf98ec
kazu7221d <mkz.dm0@gmail.com>
2018-07-08 18:47:47 +0900

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,41 @@
-worker_process 2
+worker_processes 3
 
-listen '/tmp/unicorn.sock'
-pid "/home/ec2-user/deploy/shared/tmp/pids/unicorn.pid"
+app_path = "/home/ec2-user/deploy"
 
-stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
+listen '/tmp/unicorn.sock' #1
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+
+stderr_path File.expand_path('unicorn.err.log', File.dirname(__FILE__) + '/../log')
 stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 preload_app true
+
+# use correct Gemfile on restarts
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
+end
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
+
